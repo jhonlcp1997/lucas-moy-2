f37aa03627b0397e6dc33298ee521932f4fecd62
Jhon Cruz IMM <jhonlcp1997>
2022-06-06 09:49:05 -0500

    add .editconfig & format files

---
+++ src/Paginacion.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Paginacion = () => {
+	return (
+		<div className="topbar-filter">
+			<label>Movies per page:</label>
+			<select>
+				<option value="range">5 Moviess</option>
+				<option value="saab">10 Movies</option>
+			</select>
+			<div className="pagination2">
+				<span>Page 1 of 2:</span>
+				<a className="active" href="#">1</a>
+				<a href="#">2</a>
+				<a href="#"><i className="ion-arrow-right-b"></i></a>
+			</div>
+		</div>
+	)
+}
